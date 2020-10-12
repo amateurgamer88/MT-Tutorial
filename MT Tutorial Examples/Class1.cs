11225defc8833f4801b6fbea69f5f179688c5c55
amateurgamer88 <48229872+amateurgamer88@users.noreply.github.com>
2020-10-11 18:51:30 -0700

    MT Tutorial

---
+++ MT Tutorial Examples/Class1.cs
@@ -0,0 +1,46 @@
+﻿using BepInEx;
+using HarmonyLib;
+using MonsterTrainModdingAPI.Interfaces;
+using MT_Tutorial_Examples.Cards;
+using System;
+
+[BepInPlugin("com.name.package.generic", "Test Plugin", "0.1.0")]
+[BepInProcess("MonsterTrain.exe")]
+[BepInProcess("MtLinkHandler.exe")]
+[BepInDependency("api.modding.train.monster")]
+public class TestPlugin : BaseUnityPlugin, IInitializable
+{
+    public const string GUID = "AG Tut";
+    public const string NAME = "Tutorial Examples";
+    public const string VERSION = "0.1.0";
+    private void Awake()
+    {
+        var harmony = new Harmony("AG Tut");
+        harmony.PatchAll();
+    }
+
+    public void Initialize()
+    {
+        Spell1.Make();
+    }
+}
+
+[HarmonyPatch(typeof(MetagameSaveData), "HasDiscoveredCard", new Type[] { typeof(CardData) })]
+class ShowAllLogbookCardsPatch
+{
+    static bool Prefix(ref bool __result)
+    {
+        __result = true;
+        return false;
+    }
+}
+
+[HarmonyPatch(typeof(MetagameSaveData), "HasDiscoveredCard", new Type[] { typeof(string) })]
+class ShowAllLogbookCardsPatch2
+{
+    static bool Prefix(ref bool __result)
+    {
+        __result = true;
+        return false;
+    }
+}
